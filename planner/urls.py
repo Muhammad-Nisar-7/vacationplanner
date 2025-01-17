2db9a8c151e524400fdd527bc779953ce9bdc589
Muhammad Nisar <136074406+Muhammad-Nisar-7@users.noreply.github.com>
2025-01-17 19:53:58 +0400

    Delete planner directory

--- planner/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login_view, name='login'),
-    path('home/', views.home, name='home'),
-    path('save-status/', views.save_status, name='save_status'),
-]
